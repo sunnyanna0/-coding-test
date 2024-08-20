2419e084c60942601040edb347c43a0215c7cdb6
sunny <122611609+sunnyanna0@users.noreply.github.com>
2024-08-20 18:10:39 +0000

    [Gold II] Title: 버블 소트, Time: 88 ms, Memory: 5932 KB -BaekjoonHub

---
+++ 백준/Gold/1377. 버블 소트/버블 소트.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<pair<int, int> > A(N);
+
+    for(int i=0 ; i<N ; i++){
+        cin >> A[i].first;
+        A[i].second=i;
+    }
+
+    sort(A.begin(), A.end());
+
+    int MAX=0;
+
+    for(int i=0 ; i<N ; i++){
+        if(MAX < A[i].second-i){
+            MAX=A[i].second-i;
+        }
+    }
+
+    cout << MAX+1 << endl;
+}
