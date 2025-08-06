1235a057ce64c3a62de18dfe5fb8e15736fac790
sunny <anna7789@naver.com>
2025-08-06 21:19:52 +0900

    [Gold II] Title: 저울, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2437. 저울/저울.py
+++ 백준/Gold/2437. 저울/저울.py
@@ -1,18 +1,11 @@
-import sys
-input = sys.stdin.readline
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
 
-n=int(input())
-# weight = map(int,input().split()) # 이건 한 줄 입력을 정수로 바꾸는 map 객체. map() 객체는 정렬할 수 없음
-weight = list(map(int, input().split()))
-
-weight.sort()
-
-total=0
-# for w in range(weight): #weight는 리스트이고, range(weight)는 의미 없음
-for w in weight:
-    if w<=total+1:
-        total+=w
-    else:
+target = 1
+for x in data:
+    if target < x:
         break
+    target += x
 
-print(total+1)
+print(target)
