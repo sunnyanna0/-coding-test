68a1ab94f693ef7efe548c3f5991c706f02afbc7
sunny <anna7789@naver.com>
2025-09-17 20:33:06 +0900

    [Silver III] Title: 주유소, Time: 100 ms, Memory: 48604 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,19 +2,15 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-edge = []*(n)
-node = []*(n)
-
 edge = list(map(int, input().split()))
 node = list(map(int, input().split()))
 
 money_sum = 0
-can_go = 0
-temp = 100000000
-   
+min_price = node[0]
+
 for i in range(n-1):
-    for j in range(i+1):
-        temp = min(temp, node[j])
-    money_sum += temp*edge[i]
+    if node[i] < min_price:
+        min_price = node[i]
+    money_sum += min_price * edge[i]
 
 print(money_sum)
