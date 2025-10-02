7b9495c584a378268da113564bd54988010bd864
sunny <anna7789@naver.com>
2025-10-02 11:04:55 +0900

    [Silver III] Title: 01타일, Time: 352 ms, Memory: 71968 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0]*(n+1)
+dp[0]=0
+if n>=1:
+    dp[1]=1
+if n>=2:
+    dp[2]=2
+if n>=3:
+    # print(dp[2])
+    for i in range(3, n+1):
+        dp[i]=(dp[i-1]+dp[i-2])%15746
+        # print(f"3부터는: {dp[i]}={dp[i-1]}+{dp[i-2]}")
+
+print(dp[n])
